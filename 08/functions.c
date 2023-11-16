7751de6b6e6f51d1eea0607b25de1bc3a1ff3894
Yves Chevallier <yves.chevallier@heig-vd.ch>
2023-11-16 14:46:59 +0100

    functions

---
+++ 08/functions.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+void prout() {
+    printf("prout\n");
+}
+
+int add(int a, int b) {
+    prout();
+    return a + b;
+}
+
+int main() {
+    int u = 23;
+    int v = 42;
+    printf("%d\n", add(u, v));
+    prout();
+}
+
+
