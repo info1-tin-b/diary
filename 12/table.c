4d9905ce27b49e0997c57d8901fdac715896f483
Yves Chevallier <yves.chevallier@heig-vd.ch>
2023-12-14 15:55:06 +0100

    Table

--- 12/table.c
+++ 12/table.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 int main() {
     int m[10][10] = {};
@@ -9,16 +10,18 @@ int main() {
             m[i][j] = rand() % 6000; //i * j;
             max_value = m[i][j] > max_value ? m[i][j] : max_value;
         }
-        
-    int digits = !max_value ? 1 : log10(max_value) - 1;
+
+    int digits = (!max_value ? 1 : log10(max_value) + 1) ;
+    printf("digits %d\n", digits);
     for (int i = 0; i < 10; i++) {
+        printf("| ");
         for (int j = 0; j < 10; j++) 
-            printf("%*d |", digits, m[i][j]);
-        puts("\n");
+            printf("%*d | ", digits, m[i][j]);
+        printf("\n");
         for (int j = 0; j < 10; j++) {
             printf("|");
-            printf("%.*s", digits + 1, "---------------------------------");
+            printf("%.*s", digits +2, "---------------------------------");
         }
-        puts("\n ");
+        printf("|\n");
     }
 }
