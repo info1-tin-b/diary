48ed2943f1f85bdf49ce626b1fb87f15a7e86353
Yves Chevallier <yves.chevallier@heig-vd.ch>
2024-12-17 16:24:08 +0100

    Solution bsearch

--- 13/bsearch.c
+++ 13/bsearch.c
@@ -11,12 +11,22 @@ int *bsearch(int haysack[], int size, int needle) {
     int min = 0;
     int max = size - 1;
     int mid = (max - min) / 2 + min;
-    // ...
+    while (min <= max) {
+        if (haysack[mid] == needle) {
+            return &haysack[mid];
+        } else if (haysack[mid] < needle) {
+            min = mid + 1;
+        } else {
+            max = mid - 1;
+        }
+        mid = (max - min) / 2 + min;
+    }
+    return 0;
 }
 
 int main() {
     int a[] = {2,5,13,23,42,56,73,98,103,142,166,167,168,190,200,215};
-    int *p = bsearch(a, sizeof(a)/sizeof(a[0]), 23);
+    int *p = bsearch(a, sizeof(a)/sizeof(a[0]), 190);
     if (p == 0) {
         printf("Pas trouvé\n");
     } else {
